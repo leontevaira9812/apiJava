4c783d9ca7a1b02abf571e3367ef8d785a49368b
iraleonteva <leontevaira@inbox.ru>
2023-11-14 23:21:48 +0700

    ex4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello from Ira");
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
+        response.prettyPrint();
     }
 }
