1f93d9005544abb49d478d76e973b680937e7516
iraleonteva <leontevaira@inbox.ru>
2023-11-18 13:20:59 +0700

    edited ex7

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -11,6 +11,7 @@ public class HelloWorldTest {
                 .follow(false)
                 .get("https://playground.learnqa.ru/api/long_redirect").andReturn();
         int statusCode = response.getStatusCode();
+        int count=0;
         while (statusCode != 200) {
             String returnUrl = response.getHeader("Location");
             response = RestAssured
@@ -19,7 +20,9 @@ public class HelloWorldTest {
                     .follow(false)
                     .get(returnUrl).andReturn();
             statusCode = response.getStatusCode();
-            System.out.println(returnUrl);
+            count ++;
+            System.out.println("Редирект номер " + count + " = " + returnUrl);
         }
+        System.out.println("Количество редиректов = " + count);
     }
 }
