ebca4dbeb855c5241192b1385d079d831aaca24f
iraleonteva <leontevaira@inbox.ru>
2023-11-18 12:29:25 +0700

    ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,12 +1,24 @@
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text").andReturn();
-        response.prettyPrint();
+    public void testRestAssured() {
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework").jsonPath();
+
+        ArrayList<String> message = response.get("messages.message");
+        System.out.println(message.get(1));
+
     }
 }
